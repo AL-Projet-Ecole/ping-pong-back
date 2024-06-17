b354ba0df675674057e93b4df515960f3d476c52
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-06-17 10:48:32 +0200

    CRUD opérations

---
+++ src/models/operation-repository.js
@@ -0,0 +1,37 @@
+const uuid = require('uuid');
+const Operation = require('./operation.model');
+
+exports.getOperations = async () => {
+    return await Operation.findAll();
+}
+
+exports.getOperationByLibelle = async (libelle_poste) => {
+    return await Operation.findOne({where : {libelle_operation}});
+}
+
+exports.createOperation = async (body) => {
+    const operation = body;
+    operation.id_poste = uuid.v4();
+    await Operation.create(operation);
+};
+
+exports.updateOperation = async (id_operation, data) => {
+    const foundOperation = await Operation.findOne({ where: { id_operation } });
+
+    if (!foundOperation) {
+        throw new Error('Pas de poste');
+    }
+
+    await Operation.update(
+        {
+            libelle_operation: data.libelle_operation || foundOperation.libelle_operation,
+            //TODO FAIRE LE TOUT ICI
+        },
+        { where: { libelle_operation } },
+    );
+};
+
+
+exports.deletePoste = async (id_poste) => {
+    await Operation.destroy({ where: { id_poste } });
+};
---
+++ src/models/operation.model.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./db');
+
+const realisation = sequelize.define(
+    'operations',
+    {
+        id_operation: { primaryKey: true, type: DataTypes.STRING },
+        id_poste: { foreignKey: true, type: DataTypes.STRING, allowNull: false },
+        id_machine: { foreignKey: true, type: DataTypes.STRING, allowNull: false },
+        libelle_operation: { type: DataTypes.STRING, allowNull: false },
+        temps_estimation: { type: DataTypes.STRING, allowNull: false }
+    },
+    { tableName: 'operations' },
+);
+
+module.exports = realisation;
