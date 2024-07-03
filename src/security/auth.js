5ec1b3c6e253a1e34c770f2d4531edd93d42c60a
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-07-03 16:41:29 +0200

    pour platformsh 3

--- src/security/auth.js
+++ src/security/auth.js
@@ -1,5 +1,5 @@
 const { sign } = require('jsonwebtoken');
 
 exports.generateAuthToken = (id_user, label_user, role_user) => {
-    return sign({ id_user, label_user, role_user }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+    return sign({ id_user, label_user, role_user }, "PING PONG", { expiresIn: process.env.JWT_EXPIRES_IN });
 };
