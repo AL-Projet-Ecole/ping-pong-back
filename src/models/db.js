85a0ae821b8b40c4156b90e074eb3dfef7dda862
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-07-03 16:03:26 +0200

    pour platformsh 2

--- src/models/db.js
+++ src/models/db.js
@@ -1,7 +1,22 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(`${process.env.CONNECTION_STRING}`);
+
+var dsn = process.env.CONNECTION_STRING
+if (dsn === undefined) {
+    const { env } = process;
+    const read_base64_json = function(varName) {
+        try {
+            return JSON.parse(Buffer.from(env[varName], "base64").toString())
+        } catch (err) {
+            throw new Error(`no ${varName} environment variable`)
+        }
+    };
+    const variables = read_base64_json('PLATFORM_VARIABLES')
+    dsn = variables["CONNECTION_STRING"]
+}
+console.log(dsn);
+const sequelize = new Sequelize(dsn);
 
 sequelize.authenticate()
     .then(() => {
